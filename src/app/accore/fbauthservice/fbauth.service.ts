b2cf6da3e011d0a9608f634449ffc8c0f480d0d3
natrayanp <natrayanp@gmail.com>
2018-10-27 00:37:59 +0800

    login authorise

--- src/app/accore/fbauthservice/fbauth.service.ts
+++ src/app/accore/fbauthservice/fbauth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase';
+import { auth } from 'firebase/app';
 
 
 @Injectable({
@@ -11,8 +11,8 @@ export class FbauthService {
   constructor(public afAuth: AngularFireAuth) { }
 
   resetPassword(email: string) {
-    const fbAuth = auth();
-    return fbAuth.sendPasswordResetEmail(email);
+    //const fbAuth = auth();
+    return this.afAuth.auth.sendPasswordResetEmail(email);
+    //return fbAuth.sendPasswordResetEmail(email);
   }
-
 }
